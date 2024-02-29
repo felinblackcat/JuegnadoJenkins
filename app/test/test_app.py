23796c6b82204ea654e5b38e19d9d50d7ee663f2
felinblackcat <felinandres1992@gmail.com>
2024-02-29 23:09:03 +0000

    jd

--- app/test/test_app.py
+++ app/test/test_app.py
@@ -4,7 +4,7 @@ from app.app import app
 client = TestClient(app)
 # pytest --cov=. --cov-fail-under=90
 # def coverage = sh(script: "docker run juegnadojenkins pytest --junitxml=./test.xml --cov=. --cov-fail-under=90 | grep TOTAL | awk '{ print \$4 }'", returnStdout: true).trim()
-# echo coverage
+# echo coverage -v ./app:/app 
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
