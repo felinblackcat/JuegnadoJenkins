331ae656cdfe87df820eca9f7d2bf6cf994f34f7
felinblackcat <felinandres1992@gmail.com>
2024-02-29 19:41:34 +0000

    d

--- app/test/test_app.py
+++ app/test/test_app.py
@@ -2,7 +2,7 @@ from fastapi.testclient import TestClient
 from app.app import app
 
 client = TestClient(app)
-
+# pytest --cov=. --cov-fail-under=90
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
